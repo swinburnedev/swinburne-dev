fd36b59600d6a25bc351f69a052a37847c7b78aa
Stackbit <projects@stackbit.com>
2020-11-29 21:23:27 +0000

    initial commit by Stackbit

---
+++ studio/schemas/header.js
@@ -0,0 +1,72 @@
+export default {
+    "type": "object",
+    "name": "header",
+    "title": "Header Configuration",
+    "fields": [
+        {
+            "type": "string",
+            "name": "title",
+            "title": "Header Title",
+            "description": "The title displayed in the header if no logo image added.",
+            "validation": null
+        },
+        {
+            "type": "image",
+            "name": "logo_img",
+            "title": "Logo Image",
+            "description": "The logo image displayed in the header.",
+            "validation": null
+        },
+        {
+            "type": "boolean",
+            "name": "has_nav",
+            "title": "Enable Navigation Menu",
+            "description": "Display the navigation menu bar in the header.",
+            "initialValue": true,
+            "validation": null
+        },
+        {
+            "type": "array",
+            "name": "nav_links",
+            "title": "Navigation Links",
+            "description": "List of navigation links.",
+            "validation": null,
+            "of": [
+                {
+                    "type": "action"
+                }
+            ]
+        },
+        {
+            "type": "string",
+            "name": "type",
+            "title": "Object Type",
+            "description": "The type of the object",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "header"
+                ]
+            }
+        },
+        {
+            "type": "string",
+            "name": "stackbit_model_type",
+            "title": "Stackbit Model Type",
+            "description": "Stackbit model type",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "object"
+                ]
+            }
+        }
+    ],
+    "preview": {
+        "select": {
+            "title": "title"
+        }
+    }
+}
---
+++ studio/schemas/post.js
@@ -0,0 +1,105 @@
+export default {
+    "type": "document",
+    "name": "post",
+    "title": "Post",
+    "fields": [
+        {
+            "type": "string",
+            "name": "title",
+            "title": "Title",
+            "description": "The title of the post",
+            "validation": Rule => Rule.required()
+        },
+        {
+            "type": "string",
+            "name": "subtitle",
+            "title": "Subtitle",
+            "description": "The text shown just below the post title.",
+            "validation": null
+        },
+        {
+            "type": "date",
+            "name": "date",
+            "title": "Date",
+            "validation": Rule => Rule.required()
+        },
+        {
+            "type": "image",
+            "name": "thumb_image",
+            "title": "Featured Image",
+            "description": "The image shown in the blog feed.",
+            "validation": null
+        },
+        {
+            "type": "image",
+            "name": "image",
+            "title": "Alternative Featured Image",
+            "description": "The image shown in the single post.",
+            "validation": null
+        },
+        {
+            "type": "string",
+            "name": "excerpt",
+            "title": "Excerpt",
+            "description": "The excerpt of the post displayed in the blog feed and in meta data.",
+            "validation": null
+        },
+        {
+            "type": "string",
+            "name": "layout",
+            "title": "Layout",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "post"
+                ]
+            }
+        },
+        {
+            "type": "string",
+            "name": "stackbit_url_path",
+            "title": "URL Path",
+            "description": "The URL path of this page relative to site root. For example, the site root page would be \"/\", and post page would be \"posts/new-post/\"",
+            "validation": Rule => Rule.required()
+        },
+        {
+            "type": "string",
+            "name": "stackbit_dir",
+            "title": "Directory",
+            "description": "The directory path where this file is stored",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "content/pages"
+                ]
+            }
+        },
+        {
+            "type": "markdown",
+            "name": "content",
+            "title": "Content",
+            "description": "Page content",
+            "validation": null
+        },
+        {
+            "type": "string",
+            "name": "stackbit_model_type",
+            "title": "Stackbit Model Type",
+            "description": "Stackbit model type",
+            "hidden": false,
+            "validation": Rule => Rule.required(),
+            "options": {
+                "list": [
+                    "page"
+                ]
+            }
+        }
+    ],
+    "preview": {
+        "select": {
+            "title": "title"
+        }
+    }
+}
